a34ba3705893a8414c658df93b18be568cf7fe34
Damian@Vu <minhvdq@gmail.com>
2025-04-09 16:35:53 -0400

    initialize project

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+// import { useState, useEffect } from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from './components/Home/HomePage'
+
+function App() {
+
+  return (
+    
+    <BrowserRouter>
+      <Routes>
+        <Route path={`/`} element={<HomePage />} />
+      </Routes>
+    </BrowserRouter>
+    
+  )
+  
+}
+
+export default App
